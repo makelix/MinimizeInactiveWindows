aec89ab0f824a377ae0a215c1243feae375aa4af
Marko Haverinen <marko.haverinen@proton.me>
2025-07-30 19:07:16 +0300

    Implement centering of active window.

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,6 +1,25 @@
-registerShortcut("MinimizeInactiveWindows", "Minimize Inactive Windows", "Ctrl+Alt+Home", function () {
+function minimizeInactiveWindows() {
+    // read configuration
+    var centerActiveWindow = readConfig("centerActiveWindow", false);
+    var minimizeFromAllScreens = readConfig("minimizeFromAllScreens", false);
+
+    // minimize inactive windows
     workspace.windowList().forEach(function (window) {
         if (!window.active)
             window.minimized = true;
     });
-});
+
+    // center the active window if centerActiveWindow is on
+    if (centerActiveWindow) {
+        var window = workspace.activeWindow;
+        var screenSize = window.output.geometry;
+        window.frameGeometry = {
+            x: (screenSize.width - window.width) / 2,
+            y: (screenSize.height - window.height) / 2,
+            width: window.width,
+            height: window.height
+        }
+    }
+}
+
+registerShortcut("MinimizeInactiveWindows", "Minimize Inactive Windows", "Ctrl+Alt+Home", minimizeInactiveWindows);
